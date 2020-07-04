8be3617a880859a7f2aadc73e5b0ea978f2d90f9
wizzy akor <kluverdev@gmail.com>
2020-07-04 07:32:42 -0400

    trying out vcs for intellij again

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,14 +4,11 @@ import java.text.NumberFormat;
 import java.util.Scanner;
 
 public class Main {
+    final static byte MONTHS_IN_YEAR = 12;
+    final static byte PERCENT = 100;
 
     public static void main(String[] args) {
-	// FORMAT NUMBERS IN JAVA. NumberFormat is an abstract class. you cant create objects with the 'new' keyword
-      //  String percent = NumberFormat.getPercentInstance().format(0.2);
-      //  System.out.println(percent);//shift + f6 to rename
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Age ");
-        byte age = scanner.nextByte();
-        System.out.println("you are" + age);
+
     }
+
 }
